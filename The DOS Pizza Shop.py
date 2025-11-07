b9efb03447f3e9f9a7bd2c5d2a476f4862fe9b40
SleepySlothXD <augiejuricic@gmail.com>
2025-11-07 16:41:59 -0600

    Uploaded

---
+++ The DOS Pizza Shop.py
@@ -0,0 +1,36 @@
+print("Hello there welcome to the DOS Pizza imporium! Can i get your name for the order?\n")
+
+name = input()
+menu = str("Cheese, Sausage, Pepperroni")
+print("Nice to meet you " + name + " Here is a menu just let me know what you would like when your ready\n" + menu )
+
+
+PizzaType = input()
+
+if PizzaType == "cheese" or "sausage" or "pepperroni":
+    PizzaSize = input("Okay what size do you want? We have Small, Medium, or Large\n")
+else: print("sorry but we don't have that we only have" + menu )
+    
+if PizzaSize == "small" or "medium" or "large":
+    print("Ok")
+else: print("Sorry we don't have that we only have Small, Medium, or Large")
+
+if PizzaType == "cheese":
+    pricetype = 5
+elif PizzaType == "sausage":
+    pricetype = 10
+elif PizzaType == "pepperroni":
+    pricetype = 12
+
+if PizzaSize == "small":
+    price = pricetype + 5
+elif PizzaSize == "medium":
+    price = pricetype + 10
+elif PizzaSize == "large":
+    price = pricetype + 12 
+
+payment = input("Okay for the " + PizzaSize + " " + PizzaType + " pizza, Your total is: $" + str(price) + "\nJust say Pay to confirm your order\n")
+
+if payment == "pay":
+    print("ok your order will bet out in a moment")
+    print("2 minutes later\n" + PizzaSize + " " + PizzaType + " pizza for " + name + "!!!")
